da1342f06cdfceeeb426ebeed67d8a7c02daa1ed
Kbssa11 <60266764+Kbssa11@users.noreply.github.com>
2026-09-03 07:30:25 -0300

    App Meus Jogos - versao inicial (jogos-v18)

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='jogos-v18';
+const ARQS=['./','./index.html','./matematica.html','./portugues.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ARQS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
