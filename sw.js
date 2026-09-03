de323242a59e429880e4215cb318e92b45342bfe
Kbssa11 <60266764+Kbssa11@users.noreply.github.com>
2026-09-03 08:22:23 -0300

    Menu em grade + abas Numeros e Sílabas Difíceis no Meu Ajudante (jogos-v19)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jogos-v18';
+const CACHE='jogos-v19';
 const ARQS=['./','./index.html','./matematica.html','./portugues.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ARQS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
